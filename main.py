13d808e0e98097ecca177b4a1309760073c8bcc1
GoldenbyteGH <48603774+GoldenbyteGH@users.noreply.github.com>
2022-01-04 17:03:34 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,31 +1,18 @@
-#Import Jinja2 and PyYAML
+
 from jinja2 import Environment,FileSystemLoader #Jinja work with template and python,is supermega useful 4 NetworkAutomation
 import yaml
 #declare template enviroment
 ENV = Environment(loader=FileSystemLoader('.'))
 
-def get_interface_speed(interface_name):
-    """
-    get interafce_speed returns the default Mbps value for a given network interface by looking for certain keywords in the name
+template = ENV.get_template("template.j2")      # template IN
+f_conf = open("config.txt", 'w')                #  config  OUT
 
+if __name__ == "__main__":
+    with open("devdata.yml") as f:
+        devices = yaml.safe_load(f)
 
-    Add speed {{interface.name|get_interface_speed}} to temmplate after {%endif%}
-    """
-    if 'gigabit' in interface_name.lower():
-        return 1000
-    if 'fast' in interface_name.lower():
-        return 100
-# class NetworkInterface(object):
-#     def __init__(self,name,description,vlan,uplink=False):
-#         self.name = name
-#         self.description = description
-#         self.vlan = vlan
-#         self.uplink = uplink
-#
-# #interface_obj = NetworkInterface("GigabitEthernet0/1","Server Port",10)
-#ENV.filters['get_interface_speed'] = get_interface_speed
-template = ENV.get_template("template.j2")
+    for line in template.render(device_list=devices):
+        f_conf.writelines(line)
 
-with open("data.yml") as f:
-    interfaces = yaml.safe_load(f)
-    print(template.render(interface_list=interfaces))
+    f_conf.close()
+
