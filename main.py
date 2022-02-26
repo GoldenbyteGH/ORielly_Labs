16a2f0bd365b9ce452dd726bd4b447b30fb1032e
GoldenbyteGH <48603774+GoldenbyteGH@users.noreply.github.com>
2022-02-26 18:57:50 +0100

    Update main.py

--- main.py
+++ main.py
@@ -8,10 +8,12 @@ template = ENV.get_template("template.j2")      # template IN
 f_conf = open("config.txt", 'w')                #  config  OUT
 
 if __name__ == "__main__":
-    with open("devdata.yml") as f:
-        devices = yaml.safe_load(f)
+    with open("devdata.yml") as f_dev:
+        devices = yaml.safe_load(f_dev)
+    with open("envdata.yml") as f_env:
+        srv_env = yaml.safe_load(f_env)
 
-    for line in template.render(device_list=devices):
+    for line in template.render(device_list=devices,env_data=srv_env):
         f_conf.writelines(line)
 
     f_conf.close()
